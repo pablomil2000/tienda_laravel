245550e12fef077259c0fe1bf1450b51b30d709b
pablomil2000 <81068583+pablomil2000@users.noreply.github.com>
2022-01-11 13:48:49 +0100

    Initial commit

---
+++ database/migrations/2022_01_11_123029_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('intro_description');
+            $table->string('full_description')->nullable();
+            $table->float('price');
+            $table->string('img')->nullable();
+            $table->foreignid('category_id')->constraint();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
